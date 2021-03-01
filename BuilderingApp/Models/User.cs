aa26fc5691810f7c6cad34006990099015cfc11a
but <buty.wspinaczkowe@gmail.com>
2021-03-01 13:15:19 +0000

    Added User class

---
+++ BuilderingApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuilderingApp.Models
+{
+    public class User
+    {
+        List<Route> Routes { get; set; }
+    }
+}
