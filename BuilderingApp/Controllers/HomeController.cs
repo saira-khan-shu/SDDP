9d859e6ff4c57df85a78fc319de943ec0d7a3035
buty <buty.wspinaczkowe@gmail.com>
2021-03-10 19:45:24 +0000

    Viewbag with a list of routes added to index ActionResult

--- BuilderingApp/Controllers/HomeController.cs
+++ BuilderingApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuilderingApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace BuilderingApp.Controllers
 {
     public class HomeController : Controller
     {
+        private RouteDBContext db = new RouteDBContext();
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Routes = db.Routes.ToList(); 
+            return View(); // try to provide routes here somehow 
         }
 
         public ActionResult About()
